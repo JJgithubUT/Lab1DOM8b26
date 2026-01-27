24deb3f6b31dc77ae6e7d05742e90547ac493b71
Gaby Nieva <gaby.nieva@outlook.com>
2026-01-27 11:57:56 -0600

    Sesión 1: Recuperando elementos del DOM

--- app.js
+++ app.js
@@ -7,3 +7,24 @@ const $$ = (sel, root = document) => Array.from(root.querySelectorAll(sel));
 const estadoUI = $('#estadoUI');
 const setEstado = (msg) => { estadoUI.textContent = msg; };
 setEstado('Listo');
+
+// Referencias a elementos del DOM
+const btnCambiarMensaje = $('#btnCambiarMensaje');
+const titulo = $('#tituloPrincipal');
+const subtitulo = $('#subtitulo');
+
+// Manejador de eventos
+btnCambiarMensaje.addEventListener('click', () => {
+    const alt = titulo.dataset.alt === '1';
+
+    titulo.textContent = alt
+        ? '¡Bienvenido a la Aplicación!'
+        : '¡Hola, mundo!';
+
+    subtitulo.textContent = alt
+        ? 'Explora las funcionalidades disponibles.'
+        : 'Hoy veremos cómo manipular el DOM.';
+
+    titulo.dataset.alt = alt  ? '0' : '1';
+    setEstado('Textos actualizados');
+});
